d2cc5fc2a23b9a3bfa3b013c7a67d1b73c049527
David Nguyen <davidnguyen234@gmail.com>
2022-06-02 09:59:23 -0700

    create signup form

--- urban-trails/src/components/Signup.jsx
+++ urban-trails/src/components/Signup.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
     return (
-        <div>Signup</div>
+        <div>
+            <div>
+                <h1>Sign Up</h1>
+            </div>
+            <form>
+                <div>
+                    <label>Email Address</label>
+                    <input type="email"/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password"/>
+                </div>
+                <button>Sign Up</button>
+                <p>
+                    Already have an account? <Link to="/" classname="underline">Sign in</Link> 
+                </p>
+            </form>
+        </div> 
     )
 }
 
